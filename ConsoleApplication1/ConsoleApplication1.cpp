ffaa825802d3fa71358119c22cd3a98e2968f8f0
Kirill Kapelev <kapelevkirill@gmail.com>
2025-04-04 15:22:56 +0300

    убрал коменты

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,48 +1,29 @@
-﻿//#include <iostream>
-//using namespace std;
-//
-//int max_of_three(int a, int b, int c) {
-//    int max = a;
-//    if (b > max) max = b;
-//    if (c > max) max = c;
-//    return max;
-//}
-//
-//double max_of_three(double a, double b, double c) {
-//    double max = a;
-//    if (b > max) max = b;
-//    if (c > max) max = c;
-//    return max;
-//}
-//
-//double max_of_three(int a, double b, float c) {
-//    double max = a;
-//    if (b > max) max = b;
-//    if (c > max) max = c;
-//    return max;
-//}
-//
-//int main() {
-//    cout << max_of_three(5, 3.7, 2.9f) << endl;
-//    cout << max_of_three(10, 20, 30) << endl;
-//    return 0;
-//}
+﻿#include <iostream>
+using namespace std;
 
-//#include <iostream>
-//
-//int findMax(const int arr[], int size) {
-//    if (size <= 0) return 0;
-//
-//    int max = arr[0];
-//    for (int i = 1; i < size; ++i) {
-//        if (arr[i] > max) max = arr[i];
-//    }
-//    return max;
-//}
-//
-//int main() {
-//    int arr[] = { 5, -3, 10, 7, 2 };
-//    std::cout << findMax(arr, 5) << std::endl;
-//    return 0;
-//}
+int max_of_three(int a, int b, int c) {
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    return max;
+}
 
+double max_of_three(double a, double b, double c) {
+    double max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    return max;
+}
+
+double max_of_three(int a, double b, float c) {
+    double max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    return max;
+}
+
+int main() {
+    cout << max_of_three(5, 3.7, 2.9f) << endl;
+    cout << max_of_three(10, 20, 30) << endl;
+    return 0;
+}
